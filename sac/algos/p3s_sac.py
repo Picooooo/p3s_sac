27cb53e488eb6a93434f64007ef3d2febd70d290
Kaito-Kido <maidaialam2001@gmail.com>
2022-05-19 01:48:11 +0700

    edit

--- sac/algos/p3s_sac.py
+++ sac/algos/p3s_sac.py
@@ -257,9 +257,9 @@ class P3S_sac(MARLAlgorithm, Serializable):
         # min_target_qf_t = tf.minimum(arr_target_qf_t[0], arr_target_qf_t[1])
 
         with tf.variable_scope('target'):
-            vf_next_target_t = actor._vf.get_output_for(
+            vf_next_target_t = actor.vf.get_output_for(
                 self._dict_ph['next_observations_ph'])  # N
-            actor._vf_target_params = actor.target_vf_params()
+            actor.vf_target_params = actor.target_vf_params()
 
         ys = tf.stop_gradient(self.scale_reward * self._dict_ph['rewards_ph'] +
                               (1 - self._dict_ph['terminals_ph']) *
